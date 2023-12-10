d458562e78c9ee42c9e32af4c624ee20ff5005ab
PARSA <parsazahedian84@gmail.com>
2023-12-10 11:53:31 +0330

    open_Database_and_start_logic_of_LoginGroupbox

--- login_or_signup_page.cpp
+++ login_or_signup_page.cpp
@@ -1,11 +1,21 @@
 #include "login_or_signup_page.h"
 #include "ui_login_or_signup_page.h"
+#include "QSqlDriver"
+#include "QSqlQuery"
+#include "QSqlQueryModel"
+#include "QDebug"
+
 
 Login_or_SignUp_page::Login_or_SignUp_page(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::Login_or_SignUp_page)
 {
     ui->setupUi(this);
+
+    QSqlDatabase database;
+    database=QSqlDatabase::addDatabase("QSQLITE");
+    database.setDatabaseName("e:\\schema2.db");
+    database.open();
 }
 
 Login_or_SignUp_page::~Login_or_SignUp_page()
